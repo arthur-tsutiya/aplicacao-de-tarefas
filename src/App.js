6694d4ce323cbce3103504519f04ebe7bfa5ee42
arthur-tsutiya <139712165+arthur-tsutiya@users.noreply.github.com>
2025-11-25 20:33:19 -0300

    Add basic routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header.js';
 import Footer from './Footer.js';
+import Home from './Home.js';
+import Tasks from './Tasks.js';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header/>
-      <main></main>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/tasks" element={<Tasks/>}/>
+          <Route path="*" element={<p>404 not found</p>}/>
+        </Routes>
+      </main>
       <Footer/>
-    </>
+    </BrowserRouter>
   );
 }
 
