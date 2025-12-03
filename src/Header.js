d1bfb0201fcfbc11e34f8e837cb1a0ab0404ac67
arthur-tsutiya <139712165+arthur-tsutiya@users.noreply.github.com>
2025-12-02 22:03:12 -0300

    Add initial page header structure

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
+import './Header.css';
 
 export default function Header() {
 
     return (
-        <header>
-            <nav>
-                <ul>
+        <header className="page-header">
+            <Link to="/"><h1 className="page-heading">Tasks</h1></Link>
+            <nav className="page-nav">
+                <ul className="nav-links">
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="tasks">Tasks</Link></li>
                 </ul>
