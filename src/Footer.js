b0cb2bcc9e614e8fbf8907ab180f474d123c5687
arthur-tsutiya <139712165+arthur-tsutiya@users.noreply.github.com>
2025-12-04 20:37:31 -0300

    Add basic footer to the page

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,12 @@
+import './Footer.css';
+
 export default function Footer() {
     
     return (
-        <footer></footer>
+        <footer className="page-footer">
+            <p>
+                <a className="footer-link" href="https://github.com/arthur-tsutiya/" target="_blank" rel="noopener noreferrer">Github</a>
+            </p>
+        </footer>
     );
 }
