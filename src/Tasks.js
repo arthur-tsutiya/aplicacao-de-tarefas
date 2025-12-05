d2ae87de2e48f78728da2b03862acb3121c6ce03
arthur-tsutiya <139712165+arthur-tsutiya@users.noreply.github.com>
2025-12-05 19:56:03 -0300

    Create basic Tasks page component structure

--- src/Tasks.js
+++ src/Tasks.js
@@ -1,7 +1,12 @@
+import TasksSidebar from './components/TasksSidebar';
+import TasksView from './components/TasksView'; 
 
 export default function Tasks() {
 
     return (
-        <p>Tasks page</p>
+    <>
+        <TasksSidebar/>
+        <TasksView/>
+    </>
     );
 }
---
+++ src/components/TasksSidebar.js
@@ -0,0 +1,6 @@
+
+export default function TasksSidebar() {
+    return (
+        <>test</>
+    );
+}   
