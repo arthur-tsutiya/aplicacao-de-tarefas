4addcf62d23b1b5965e278c16da953997e8e3504
arthur-tsutiya <139712165+arthur-tsutiya@users.noreply.github.com>
2025-12-06 20:41:50 -0300

    Add the ability to complete a task

--- src/Tasks.js
+++ src/Tasks.js
@@ -6,16 +6,32 @@ import { useState } from 'react';
 const initialTasks = [
     {id: 1, description: 'Buy milk', done: false},
     {id: 2, description: 'Clean the fridge', done: false},
-    {id: 3, description: 'Fix sleeping schedule', done: true}
+    {id: 3, description: 'Fix sleeping schedule', done: true},
+    {id: 4, description: 'Take out the trash', done: false},
+    {id: 5, description: 'Call mom', done: false},
+    {id: 6, description: 'Set up appointment', done: false},
 ]
 
 export default function Tasks() {
     const [tasks, setTasks] = useState(initialTasks);
 
+    function toggleTask(id) {
+        setTasks(tasks.map(task => {
+            if (task.id !== id) {
+                return task;
+            }
+
+            return {
+                ...task,
+                done: !task.done
+            };
+        }));
+    }
+
     return (
     <main className="main-page tasks-wrapper">
         <TasksSidebar/>
-        <TasksView tasks={tasks}/>
+        <TasksView tasks={tasks} onTaskToggle={toggleTask}/>
     </main>
     );
 }
