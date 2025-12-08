0616840a76c78b2e710361db349a44acaa5a031d
arthur-tsutiya <139712165+arthur-tsutiya@users.noreply.github.com>
2025-12-08 18:55:01 -0300

    Fix bug in task rendering

--- src/components/TasksView.js
+++ src/components/TasksView.js
@@ -7,22 +7,23 @@ import { useState } from 'react';
 export default function TasksView({tasks, onTaskToggle, onTaskChange, onTaskDelete, onTaskAdd, onTaskImportanceToggle}) {
     const [editedTaskId, setEditedTaskId] = useState(null);
     const [newTask, setNewTask] = useState(false);
-/*
+
     const importantTasks = [];
     const regularTasks = [];
     tasks.forEach(task => {
-      if (task.important) {
-        let element = <Task key={task.id} task={task} onTaskToggle={onTaskToggle} onEditClick={editTask} onTaskDelete={onTaskDelete} onTaskImportanceToggle={onTaskImportanceToggle} important/>;
+
+        let element = <Task key={task.id} task={task} onTaskToggle={onTaskToggle} onEditClick={editTask} onTaskDelete={onTaskDelete} onTaskImportanceToggle={onTaskImportanceToggle}/>;
         if (task.id === editedTaskId) {
           element = <TaskEdit task={task} onTaskChange={onTaskChange} onTaskEditEnd={finishEditTask} onTaskImportanceToggle={onTaskImportanceToggle}/>;
         }
+
+      if (task.important) {
         importantTasks.push(element);
       } else {
-        let element = <Task key={task.id} task={task} onTaskToggle={onTaskToggle} onEditClick={editTask} onTaskDelete={onTaskDelete} onTaskImportanceToggle={onTaskImportanceToggle}/>;
         regularTasks.push(<Task key={task.id} task={task} onTaskToggle={onTaskToggle} onEditClick={editTask} onTaskDelete={onTaskDelete} onTaskImportanceToggle={onTaskImportanceToggle}/>);
       }
     }); 
-*/
+
     function editTask(id) {
       setEditedTaskId(id);
     }
@@ -43,10 +44,8 @@ export default function TasksView({tasks, onTaskToggle, onTaskChange, onTaskDele
       <section className="tasks-view">
         {newTask === true ? <TaskNew onTaskAdd={onTaskAdd} onFinishedTaskAdd={finishCreatingTask} /> : <button className="task card card-btn" onClick={createTask}>Add task</button>}
         <ul className="tasks-list">
-            {tasks.toReversed().map(task => {
-                if (editedTaskId === task.id) return <TaskEdit task={task} onTaskChange={onTaskChange} onTaskEditEnd={finishEditTask} onTaskImportanceToggle={onTaskImportanceToggle}/>;
-                return <Task key={task.id} task={task} onTaskToggle={onTaskToggle} onEditClick={editTask} onTaskDelete={onTaskDelete} onTaskImportanceToggle={onTaskImportanceToggle}/>;
-            })}
+            {importantTasks}
+            {regularTasks}
         </ul>
       </section>
     );
