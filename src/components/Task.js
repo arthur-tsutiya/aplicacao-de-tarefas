74c4df816af6e2884984d96f23a20a5ac6ae9b97
arthur-tsutiya <139712165+arthur-tsutiya@users.noreply.github.com>
2025-12-08 13:36:02 -0300

    Improve the style of the tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,17 +2,20 @@ import './Task.css';
 
 export default function Task({task, onTaskToggle, onEditClick, onTaskDelete}) {
 
-    let taskClassName = "";
+    let taskClassName = "task-title";
     if (task.done) {
-        taskClassName += "done-task";
+        taskClassName += " done-task";
     }
 
     return (
         <li className="task card">
             <button onClick={() => onTaskToggle(task.id)}>done</button>
             <p className={taskClassName}>{task.description}</p>
-            <button onClick={() => onEditClick(task.id)}>Edit</button>
-            <button onClick={() => onTaskDelete(task.id)}>Delete</button>
+            <div className="task-buttons">
+                <button onClick={() => onEditClick(task.id)}>Edit</button>
+                <button onClick={() => onTaskDelete(task.id)}>Delete</button>
+                <button className="btn-important-task" onClick={() => {}}>Favorite</button>
+            </div>
         </li>
     );
 }
