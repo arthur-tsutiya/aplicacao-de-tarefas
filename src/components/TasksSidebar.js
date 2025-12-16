3b70d0a22d031fbf58df5ce13c8a1ab69698b888
arthur-tsutiya <139712165+arthur-tsutiya@users.noreply.github.com>
2025-12-15 23:22:03 -0300

    Create the initial structure for the sidebar in the 'Tasks' page

--- src/components/TasksSidebar.js
+++ src/components/TasksSidebar.js
@@ -1,14 +1,33 @@
 import './TasksSidebar.css';
+import TasksIcon from './icons/TasksIcon.js';
+import StarIcon from './icons/StarIcon.js';
 
 export default function TasksSidebar() {
+
+
+
     return (
         <section className="tasks-sidebar">
             <nav>
-                <ul>
-                    <li className="tasks-sidebar-item">
-                        <button>
-                            <span></span>
-                            <span></span>
+                <ul className="sidebar-list">
+                    <li className="sidebar-list-item">
+                        <button className="btn sidebar-item-btn">
+                            <div className="sidebar-item-icon center">
+                                <TasksIcon />
+                            </div>
+                            <span className="sidebar-item-text">
+                                All tasks
+                            </span>
+                        </button>
+                    </li>
+                    <li className="sidebar-list-item">
+                        <button className="btn sidebar-item-btn">
+                            <div className="sidebar-item-icon center">
+                                <StarIcon />
+                            </div>
+                            <span className="sidebar-item-text">
+                                Important Tasks
+                            </span>
                         </button>
                     </li>
                 </ul>
