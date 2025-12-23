738ee29b543f6781311ca405ef03e3a30e826055
arthur-tsutiya <139712165+arthur-tsutiya@users.noreply.github.com>
2025-12-22 22:32:47 -0300

    Create a basic Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,30 @@
 import Footer from '../components/Footer.js';
+import './Home.css';
+import { Link } from 'react-router-dom';
+import { useMediaQuery } from '../contexts/MediaQueryProvider.js';
 
 export default function Home() {
+    const mediaQuery = useMediaQuery();
     
     return (
         <>
-            <main className="main-page container">
-                <p>Home page (in construction)</p>
+            <main className="main-page home-page container">
+                <section className="hero-section">
+                    <div className="hero-section-text">
+                        <h1 className="hero-main-heading hero-heading">Welcome to Tasks!</h1>
+                        <h2 className="hero-subheading hero-heading">The best place to manage and keep track of your day-to-day tasks.</h2>
+                        <p className="hero-text">
+                            To start, click {' '}
+                            <Link className="hero-text-link" to="/tasks">here</Link>
+                        </p>
+                    </div>
+                    {mediaQuery !== "mobile" && <div className="hero-section-image center">
+                        hero image placeholder
+                    </div>}
+                </section>
+                <section>
+                    <p className="author-note" style={{textAlign: "center", marginBlock: "10em"}}>Page still under construction!</p>
+                </section>
             </main>
             <Footer/>
         </>
